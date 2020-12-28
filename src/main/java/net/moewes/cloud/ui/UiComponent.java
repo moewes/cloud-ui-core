6e5e0e607f98665772a8c6591a3f034c41127a68
Maik Möwes <moewes@users.noreply.github.com>
2020-12-28 17:30:55 +0100

    Better input handling

--- src/main/java/net/moewes/cloud/ui/UiComponent.java
+++ src/main/java/net/moewes/cloud/ui/UiComponent.java
@@ -1,126 +1,137 @@
 package net.moewes.cloud.ui;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
-import java.util.function.Function;
+
 import lombok.Getter;
-import lombok.Setter;
 
 
 public class UiComponent {
 
-  private final String tag;
-  @Getter
-  private String id;
-  private List<UiComponent> children = new ArrayList<>();
-  private UiElement uiElement;
-  private UiBinder binder;
-  private Consumer<String> eventHandler;
+    private final String tag;
+    @Getter
+    private String id;
+    private List<UiComponent> children = new ArrayList<>();
+    private UiElement uiElement;
+    private UiBinder binder;
+    private Consumer<String> eventHandler;
 
-  public UiComponent() {
-    this("div");
-  }
-
-  public UiComponent(String tag) {
-    this.tag = tag;
-    this.id = this.getClass().getName();
-    uiElement = new UiElement(tag);
-    uiElement.setId(this.getId());
-  }
-
-  public void bind(UiBinder binder) {
-    this.binder = binder;
-  }
-
-  public void unbind() {
-    binder = null;
-  }
-
-  public void add(UiComponent component) {
-    children.add(component);
-    component.setId(getId() + "_" + children.size());
-    getElement().add(component.getElement());
-  }
-  
-  public void remove(UiComponent component) {
-    children.remove(component);
-    getElement().remove(component.getElement());
-  }
-
-  public UiElement getElement() {
-    return uiElement;
-  }
-
-  public UiElement render() {
-
-    if (children != null) {
-      children.stream().forEach(UiComponent::render);
+    public UiComponent() {
+        this("div");
     }
-    getValuesFromBinder();
-    return uiElement;
-  }
 
-  private void getValuesFromBinder() {
-    if (binder != null) {
-      binder.setValue(); // FIXME Name
+    public UiComponent(String tag) {
+        this.tag = tag;
+        this.id = this.getClass().getName();
+        uiElement = new UiElement(tag);
+        uiElement.setId(this.getId());
     }
-  }
 
-  public void setValue(String value) {
-    getElement().setValue(value);
-  }
-
-  public void setValuesWithBinder() {
-    if (binder != null) {
-      binder.getValue(); // FIXME Name
+    public void bind(UiBinder binder) {
+        this.binder = binder;
     }
-  }
 
-  public String getValue() {
-    return getElement().getValue();
-  }
+    public void unbind() {
+        binder = null;
+    }
 
+    public void add(UiComponent component) {
+        children.add(component);
+        component.setId(getId() + "_" + children.size());
+        getElement().add(component.getElement());
+    }
 
-  public void setInnerHtml(String content) {
-    getElement().setInnerHtml(content);
-  }
+    public void add(UiComponent... components) {
+        Arrays.stream(components).forEach(this::add);
+    }
 
-  public Optional<UiComponent> getComponentWithId(String id) {
-    if (this.getId().equals(id)) {
-      System.out.println("found id " + id);
-      return Optional.of(this);
-    } else if (children != null) {
-      for (UiComponent item : children) {
-        Optional<UiComponent> x = item.getComponentWithId(id);
-        if (x.isPresent()) {
-          return x;
+    public void remove(UiComponent component) {
+        children.remove(component);
+        getElement().remove(component.getElement());
+    }
+
+    public UiElement getElement() {
+        return uiElement;
+    }
+
+    public UiElement render() {
+
+        if (children != null) {
+            children.stream().forEach(UiComponent::render);
         }
-      }
-      return Optional.empty();
-    } else {
-      return Optional.empty();
+        getValuesFromBinder();
+        if (uiElement.getValue() == null) {
+            uiElement.setValue(getEmptyValue());
+        }
+        return uiElement;
     }
-  }
 
-  public void addEventListener(String event, Consumer<String> function) {
-    this.eventHandler = function;
-    getElement().addEvent(event);
-  }
-
-  public void handleEvent(String click) {
-    if (eventHandler != null) {
-      eventHandler.accept(click);
+    protected String getEmptyValue() {
+        return "";
     }
-  }
 
-  public void setId(String id) {
-    this.id = id;
-    getElement().setId(getId());
+    private void getValuesFromBinder() {
+        if (binder != null) {
+            binder.setValue(); // FIXME Name
+        }
+    }
 
-    children.forEach(child -> {
-      child.setId(getId()+child.getId());
-    });
-  }
+    public void setValue(String value) {
+        getElement().setValue(value);
+    }
+
+    public void setValuesWithBinder() {
+        if (binder != null) {
+            binder.getValue(); // FIXME Name
+        }
+    }
+
+    public String getValue() {
+        return getElement().getValue();
+    }
+
+
+    public void setInnerHtml(String content) {
+        getElement().setInnerHtml(content);
+    }
+
+    public Optional<UiComponent> getComponentWithId(String id) {
+        if (this.getId().equals(id)) {
+            System.out.println("found id " + id);
+            return Optional.of(this);
+        } else if (children != null) {
+            for (UiComponent item : children) {
+                Optional<UiComponent> x = item.getComponentWithId(id);
+                if (x.isPresent()) {
+                    return x;
+                }
+            }
+            return Optional.empty();
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public void addEventListener(String event, Consumer<String> function) {
+        this.eventHandler = function;
+        getElement().addEvent(event);
+    }
+
+    public void handleEvent(String click) {
+        if (eventHandler != null) {
+            eventHandler.accept(click);
+        }
+    }
+
+    public void setId(String id) {
+        this.id = id;
+        getElement().setId(getId());
+
+        children.forEach(child -> {
+            child.setId(getId() + child.getId());
+        });
+    }
 }
--- src/main/java/net/moewes/cloud/ui/UiElement.java
+++ src/main/java/net/moewes/cloud/ui/UiElement.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,43 +12,44 @@ import lombok.Setter;
 @Setter
 public class UiElement {
 
-  private final String tag;
-  private String id;
-  private String value;
-  private String innerHtml;
-  private List<UiElement> children;
-  private List<String> events;
-  private Set<UiElementAttribute> attributes;
+    private final String tag;
+    private String id;
+    private String value;
+    private boolean hasInput;
+    private String innerHtml;
+    private List<UiElement> children;
+    private List<String> events;
+    private Set<UiElementAttribute> attributes;
 
-  public UiElement(String tag) {
-    this.tag = tag;
-    this.id = this.getClass().getName();
-  }
-
-  public void add(UiElement element) {
-    if (children == null) {
-      children = new ArrayList<>();
+    public UiElement(String tag) {
+        this.tag = tag;
+        this.id = this.getClass().getName();
     }
-    children.add(element);
-  }
 
-  public void addEvent(String event) {
-    if (events == null) {
-      events = new ArrayList<>();
+    public void add(UiElement element) {
+        if (children == null) {
+            children = new ArrayList<>();
+        }
+        children.add(element);
     }
-    events.add(event);
-  }
 
-  public void remove(UiElement element) {
-    if (children!=null) {
-      children.remove(element);
+    public void addEvent(String event) {
+        if (events == null) {
+            events = new ArrayList<>();
+        }
+        events.add(event);
     }
-  }
 
-  public void setAttribute(String key, String value) {
-    if (attributes == null) {
-      attributes = new HashSet<>();
+    public void remove(UiElement element) {
+        if (children != null) {
+            children.remove(element);
+        }
     }
-    attributes.add(new UiElementAttribute(key, value));
-  }
+
+    public void setAttribute(String key, String value) {
+        if (attributes == null) {
+            attributes = new HashSet<>();
+        }
+        attributes.add(new UiElementAttribute(key, value));
+    }
 }
