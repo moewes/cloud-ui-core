f271eaed6000003470802b644b2972b137c002d7
Maik Möwes <moewes@users.noreply.github.com>
2021-06-26 11:23:10 +0200

    Binding improvements

--- src/main/java/net/moewes/cloudui/UiBinder.java
+++ src/main/java/net/moewes/cloudui/UiBinder.java
@@ -5,31 +5,33 @@ import java.util.function.Supplier;
 
 public class UiBinder {
 
-  private Supplier<?> getter;
-  private Consumer<String> setter;
-  private UiComponent component;
+    private Supplier<?> getter;
+    private Consumer<String> setter;
+    private UiComponent component;
 
-  public void bind(UiComponent component, Supplier<?> getter,
-      Consumer<String> setter) {
+    public void bind(UiComponent component, Supplier<?> getter,
+                     Consumer<String> setter) {
 
-    this.getter = getter;
-    this.setter = setter;
-    this.component = component;
-    component.bind(this);
-  }
-
-  public void setValue() {
-
-    if (getter!=null) {
-      Object o = getter.get();
-      component.setValue(o.toString());
+        this.getter = getter;
+        this.setter = setter;
+        this.component = component;
+        component.bind(this);
     }
-  }
 
-  public void getValue() {
+    public void setValue() {
 
-    if (setter!=null) {
-      setter.accept(component.getValue());
+        if (getter != null) {
+            Object o = getter.get();
+            if (o != null) {
+                component.setValue(o.toString());
+            }
+        }
     }
-  }
+
+    public void getValue() {
+
+        if (setter != null) {
+            setter.accept(component.getValue());
+        }
+    }
 }
